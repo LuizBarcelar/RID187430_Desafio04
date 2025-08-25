13ea3dbb426d89f3158c116b523547678ad13c4f
Luiz Barcelar <luizbarcelar2014@gmail.com>
2025-08-25 11:16:43 -0300

    feat: configs material ui theme and css baseline

---
+++ src/theme.ts
@@ -0,0 +1,39 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+let theme = createTheme({
+    palette: {
+        primary: {
+            main: '#FFFFFF'
+        }
+    },
+    typography: {
+        fontFamily: "Poppins",
+        h1: {
+            fontSize: "4 rem",
+            fontWeight: "600",
+            fontStyle: "SemiBold"
+        },
+
+        h2: {
+            fontSize: "2.5 rem",
+            fontWeight: "600",
+            fontStyle: "SemiBold"
+        },
+
+        h3: {
+            fontSize: "2 rem",
+            fontWeight: "600",
+            fontStyle: "SemiBold"
+        },
+
+        h4: {
+            fontSize: "1.5 rem",
+            fontWeight: "600",
+            fontStyle: "SemiBold"
+        }
+    },
+})
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
