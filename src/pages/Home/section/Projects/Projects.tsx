f5426cd8e7b63739b11dc998a16a34614efbd55c
Luiz Barcelar <luizbarcelar2014@gmail.com>
2025-09-10 20:37:21 -0300

    feat: completion of cards

--- src/pages/Home/section/Projects/Projects.tsx
+++ src/pages/Home/section/Projects/Projects.tsx
@@ -5,32 +5,32 @@ import StyledButton from '../../../../components/StyledButton';
 
 const projects = [
   {
-    title: 'Projeto 1',
-    description: 'Este é um projeto incrível que faz XYZ.',
-    image: 'https://via.placeholder.com/400x200',
-    github: 'https://github.com/seuusuario/projeto1',
-    demo: 'https://projeto1.demo.com',
+    title: 'Landing Page',
+    description: 'Landing page moderna e responsiva feita com HTML e CSS, criada para promover um produto digital. Possui layout limpo, seção de recursos, formulário de contato e é otimizada para dispositivos móveis e SEO.',
+    image: '/image/projeto01.png',
+    github: 'https://github.com/LuizBarcelar/RID187430_Desafio02',
+    demo: 'https://noticias-desenvolvedores.netlify.app/',
   },
   {
-    title: 'Projeto 2',
-    description: 'Outro projeto interessante com React e Node.js.',
-    image: 'https://via.placeholder.com/400x200',
-    github: 'https://github.com/seuusuario/projeto2',
-    demo: '',
+    title: 'Menu Board',
+    description: 'Projeto de lista de tarefas criado para organizar atividades do dia a dia. Permite adicionar, editar, concluir e deletar tarefas. Desenvolvido com foco em manipulação de DOM (JavaScript) ou uso de estados (React). Totalmente responsivo e com salvamento local no navegador..',
+    image: '/image/projeto02.png',
+    github: 'https://github.com/LuizBarcelar/Lista-de-tarefas---DNC',
+    demo: 'https://menu-board.netlify.app/',
   },
   {
-    title: 'Projeto 3',
-    description: 'Aplicativo mobile feito com React Native.',
-    image: 'https://via.placeholder.com/400x200',
-    github: 'https://github.com/seuusuario/projeto3',
-    demo: 'https://projeto3.demo.com',
+    title: 'Sales Dashboard',
+    description: 'O Sales Dashboard é um painel analítico responsivo que permite a visualização de dados de vendas de maneira clara e objetiva. Com uma interface moderna, o projeto oferece gráficos dinâmicos, indicadores chave (KPIs) e uma funcionalidade de alteração de tema (claro/escuro) para melhor adaptação às preferências do usuário..',
+    image: '/image/projeto03.png',
+    github: 'https://github.com/LuizBarcelar/dnc-sales-dashboard-luiz',
+    demo: 'https://sweet-rolypoly-1cb4e3.netlify.app/',
   },
   {
-    title: 'Projeto 4',
-    description: 'Aplicativo mobile feito com React Native.',
-    image: 'https://via.placeholder.com/400x200',
-    github: 'https://github.com/seuusuario/projeto3',
-    demo: 'https://projeto3.demo.com',
+    title: 'Pagina Web',
+    description: 'Este projeto é uma página web responsiva desenvolvida com React e Vite, com foco em usabilidade e personalização da experiência do usuário. Entre as funcionalidades principais estão a troca dinâmica de idiomas (ex: português e inglês) e um sistema de marcação de itens como favoritos — que ficam salvos no navegador, mesmo após atualizar ou fechar a página.',
+    image: '/image/projeto04.png',
+    github: 'https://github.com/LuizBarcelar/dnc-luiz',
+    demo: 'https://comforting-gecko-5d9960.netlify.app/',
   },
 ];
 
@@ -58,6 +58,7 @@ const Projects = () => {
                 borderBottomRightRadius: '50px',
                 overflow: 'hidden',
                 mx: 'auto',
+                minHeight: 500,
                 width: {
                   xs: '90%',
                   sm: '80%',
@@ -74,22 +75,72 @@ const Projects = () => {
                       : '0 12px 48px 12px rgba(0, 0, 0, 0.45)',
                   },
                 }}>
+
+                {project.demo ? (
+                  <Box
+                    component="a"
+                    href={project.demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ display: 'flex',justifyContent: 'center', textDecoration: 'none' }}
+                  >
+                    <Box
+                      component="img"
+                      src={project.image}
+                      alt={project.title}
+                      sx={{
+                        height: 200,
+                        width: '90%',
+                        objectFit: 'cover',
+                        borderRadius: 2,
+                        border: '2px solid',
+                        borderColor: theme.palette.divider,
+                        mx: 'auto',
+                        mt: 3,
+                        transition: '0.3s',
+                        '&:hover': {
+                          opacity: 0.9,
+                          transform: 'scale(1.01)',
+                        },
+                      }}
+                    />
+                  </Box>
+                ) : (
                 <CardMedia
                   component="img"
-                  height="350"
-                  image={project.image}
+                  src={project.image}
                   alt={project.title}
+                  sx={{
+                    height: 200,
+                    width: '90%',
+                    objectFit: 'cover',
+                    borderRadius: 2,
+                    border: '2px solid',
+                    borderColor: theme.palette.divider,
+                    mx: 'auto',
+                    mt: 3,
+                  }}
                 />
+                )}
                 <CardContent>
                   <Typography variant="h6" textAlign="center" gutterBottom >
                     {project.title}
                   </Typography>
-                  <Typography variant="body2" mt={6} >{project.description}</Typography>
+                  <Typography variant="body2" mt={4} >{project.description}</Typography>
                 </CardContent>
-                <CardActions sx={{ paddingBottom: 4, paddingLeft: 2}}>
-                  <StyledButton >
-                    clique aqui
-                  </StyledButton>
+                <CardActions sx={{ marginTop: 2,  paddingLeft: 2}}>
+                  {project.github && (
+                    <a
+                      href={project.github}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ textDecoration: 'none', color: 'inherit' }}
+                    >
+                    <StyledButton >
+                      Ver código
+                    </StyledButton>
+                    </a>
+                  )}
                 </CardActions>
               </Card>
             </Grid>
